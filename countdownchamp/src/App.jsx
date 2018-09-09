3db382c63e7093b11c932f8466ab7f6b1bcf3437
indraadidaya <p.querto@google.com>
2018-09-09 21:46:54 +0700

    - update simple dynamic state

--- countdownchamp/src/App.jsx
+++ countdownchamp/src/App.jsx
@@ -2,32 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    deadline: 'September 22, 2018'
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'September 22, 2018'
+      }
   }
-}
 
- render() {
-  return  (
-  <div className="App">
-    <div className="App-title">
-      Countdown to {this.state.deadline}
+  changeDeadline() {
+    this.setState({deadline: 'September 22, 2018'})
+  }
+
+  render() {
+    return  (
+    <div className="App">
+      <div className="App-title">
+        Countdown to {this.state.deadline}
+        </div>
+      <div>
+        <div className="Clock-days">14 days</div>
+        <div className="Clock-hours">30 hours</div>
+        <div className="Clock-minutes">15 minutes</div>
+        <div className="Clock-seconds">20 seconds</div>
+      </div>
+      <div>
+        <input placeholder='new date' />
+        <button onClick={() => this.changeDeadline()}>
+          Submit
+        </button>
+      </div>
     </div>
-    <div>
-      <div className="Clock-days">14 days</div>
-      <div className="Clock-hours">30 hours</div>
-      <div className="Clock-minutes">15 minutes</div>
-      <div className="Clock-seconds">20 seconds</div>
-    </div>
-    <div>
-      <input placeholder='new date' />
-      <button>Submit</button>
-    </div>
-  </div>
-  )
- }
+    )
+  }
 }
 
 export default App;
