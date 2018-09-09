48e416e892e4b843bc69e69c5bcbcd33756be435
indraadidaya <p.querto@google.com>
2018-09-09 21:15:34 +0700

    - update simple state

--- countdownchamp/src/App.jsx
+++ countdownchamp/src/App.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    deadline: 'September 22, 2018'
+  }
+}
+
  render() {
   return  (
   <div className="App">
-    <div className="App-title">Countdown to September 22, 2018</div>
+    <div className="App-title">
+      Countdown to {this.state.deadline}
+    </div>
     <div>
       <div className="Clock-days">14 days</div>
       <div className="Clock-hours">30 hours</div>
