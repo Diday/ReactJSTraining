c399e763ce29f2fe658a09b47567ad3115843133
indraadidaya <p.querto@google.com>
2018-09-15 17:18:54 +0700

    - Adding helper methods with ternary expressions.

--- countdownchamp/src/Clock.jsx
+++ countdownchamp/src/Clock.jsx
@@ -17,28 +17,35 @@ class Clock extends Component {
   }
 
   componentDidMount() {
-  setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+    setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+  }
+
+  leadingZero(num) {
+    //if (num < 10) {
+      //return '0' + num;
+    //}
+    return num < 10 ? '0' + num : num;
   }
 
   getTimeUntil(deadline) {
     const time = Date.parse(deadline) - Date.parse(new Date());
-    console.log('time', time);
+    //console.log('time', time);
     const seconds = Math.floor((time / 1000) % 60);
     const minutes = Math.floor((time / 1000 / 60) % 60);
     const hours = Math.floor(time / (1000 * 60 * 60) % 24);
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
-    console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days);
+    //console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days);
     this.setState({days, hours, minutes, seconds});
   }
 
   render() {
     return (
       <div>
-        <div className="Clock-days">{this.state.days} days</div>
-        <div className="Clock-hours">{this.state.hours} hours</div>
-        <div className="Clock-minutes">{this.state.minutes} minutes</div>
-        <div className="Clock-seconds">{this.state.seconds} seconds</div>
+        <div className="Clock-days">{this.leadingZero(this.state.days)} days</div>
+        <div className="Clock-hours">{this.leadingZero(this.state.hours)} hours</div>
+        <div className="Clock-minutes">{this.leadingZero(this.state.minutes)} minutes</div>
+        <div className="Clock-seconds">{this.leadingZero(this.state.seconds)} seconds</div>
       </div>
     )
   }
