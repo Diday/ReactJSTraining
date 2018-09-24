629b3f2766e0fbff4bd6fd65e38900b0f656dcca
indraadidaya <p.querto@google.com>
2018-09-24 20:47:10 +0700

    - Building artist profile

--- musicmaster/src/App.jsx
+++ musicmaster/src/App.jsx
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Profile from './Profile';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-     query:''
+     query: '',
+     artist: null
     }
   }
 
 search() {
   console.log('this.state', this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+    var accessToken = 'BQAtz6GnesrC4FdNeydaE-lzfLjeA_lb7GShQILbrC5_r1OdfcVOIJjMebWBJh1hqyvsqbOSDx663hdosq3aAOCb9qr_qCzyg12CAcYVGu7Y9u8c-brAlTDLPLJTDTja7ig1WbpNMQs1i1jXndYXrxMXZrf-lQ&refresh_token=AQDfLE77Lw9R0u1J4Qur8-lddcVNb1LIGYP75c670ulwwfiHelckaR48GIeLnTv5-e3rb4CdavT3pQROXjGiUChygjiihjiEjN9Hi9pNIdC66kJjtJsBYPB6C4NSTEm1p0xkmg';
+
+    var myOptions = {
+      method: 'GET',
+      headers:  {
+        'Authorization': 'Bearer ' + accessToken
+     },
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    fetch(FETCH_URL, myOptions )
+      .then(response => response.json())
+      .then(json => {
+        const artist = json.artists.items[0];
+        this.setState({artist});
+      })
 }
 
   render() {
@@ -36,10 +57,9 @@ search() {
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
-        <div className="Profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
-        </div>
+        <Profile
+          artist={this.state.artist}
+        />
         <div className="Gallery">
           Gallery
         </div>
