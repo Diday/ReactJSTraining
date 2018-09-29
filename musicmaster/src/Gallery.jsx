660cd7777f766a1598dd8173c1a30e22b1b83c17
indraadidaya <p.querto@google.com>
2018-09-29 10:41:33 +0700

    - Play audio with JavaScript.

--- musicmaster/src/Gallery.jsx
+++ musicmaster/src/Gallery.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Gallery extends Component {
+  playAudio(previewUrl) {
+    let audio = new Audio(previewUrl);
+    audio.play()
+  }
+
   render() {
     const {tracks} = this.props;
     return (
@@ -9,7 +14,11 @@ class Gallery extends Component {
         {tracks.map((track, key) => {
           const trackImg = track.album.images[0].url;
           return (
-            <div key= {key} className= "Track">
+            <div
+              key= {key}
+              className= "Track"
+              onClick= {() => this.playAudio(track.preview_url)}
+            >
               <img src= {trackImg} className= "Track-img" alt= "Track"/>
               <p className= "Track-text">{track.name}</p>
             </div>
