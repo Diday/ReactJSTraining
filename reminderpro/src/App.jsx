1c027c568cf77e564ccf5b48dfd9d222d74ff00e
indraadidaya <p.querto@google.com>
2018-10-01 20:35:16 +0700

    - Preparing reminder pro - Install react-bootstrap

---
+++ reminderpro/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+//import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">Reminder Pro</div>
+      </div>
+    )
+  }
+}
+
+export default App;
