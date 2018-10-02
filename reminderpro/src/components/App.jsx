50655213948de4a8f4ad98e693a39019d61b7984
indraadidaya <p.querto@google.com>
2018-10-02 19:35:19 +0700

    - preparing components

--- reminderpro/src/components/App.jsx
+++ reminderpro/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './../App.css';
 //import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
@@ -7,6 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-title">Reminder Pro</div>
+        <div className="form-inline">
+          <div className="form-group">
+            <input className="form-control" placeholder="I have to.."/>
+          </div>
+          <button type="button" className="btn btn-success">
+            Add Reminder
+          </button>
+        </div>
       </div>
     )
   }
